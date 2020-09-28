f5016c3822a62acee8e1cfb7a357434678710983
Daniel Floyd <gleek.dan@gmail.com>
2020-09-28 16:01:16 -0600

    added a get single guide by id route

--- app.py
+++ app.py
@@ -52,5 +52,12 @@ def get_guides():
   return jsonify(result)
 
 
+# Endpoint for querying a single guide
+@app.route("/guide/<id>", methods=["GET"])
+def get_guide(id):
+  guide = Guide.query.get(id)
+  return guide_schema.jsonify(guide)
+
+
 if __name__ == '__main__':
   app.run(debug=True)
